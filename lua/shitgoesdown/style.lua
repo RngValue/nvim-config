9c8f10b1ef4fcdef1942ae3667f3673acf66a5f1
CursedCodMan <104089072+RngValue@users.noreply.github.com>
2025-07-06 10:26:28 +0200

    Update style.lua

--- lua/shitgoesdown/style.lua
+++ lua/shitgoesdown/style.lua
@@ -2,9 +2,9 @@ vim.cmd("colorscheme catppuccin")
 vim.cmd("set number")
 vim.cmd("set cursorline")
 
-vim.opt.tabstop = 3
-vim.opt.softtabstop = 3
-vim.opt.shiftwidth = 3
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
