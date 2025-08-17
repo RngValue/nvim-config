9953883ddcbb69d41ccadab5b51df046060db91a
CursedCodMan <104089072+RngValue@users.noreply.github.com>
2025-08-17 22:36:27 +0200

    Update remap.lua

--- lua/shitgoesdown/remap.lua
+++ lua/shitgoesdown/remap.lua
@@ -1,3 +1,10 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- quality :3
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<ESC>", vim.cmd.noh)
+vim.keymap.set({"n", "o", "x"}, "<leader>p", "\"_dP")
+
+-- quality but mobile :3
+vim.keymap.set({"n", "o", "x"}, "L", "$")
+vim.keymap.set({"n", "o", "x"}, "H", "^")
